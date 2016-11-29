3756e721283848438a2f796401614dacde1763e3
cwbs <jing.05y@gmail.com>
2016-11-29 11:14:54 +0000

    added tests to MoneyTest

--- src/roms/MoneyTest.java
+++ src/roms/MoneyTest.java
@@ -4,6 +4,9 @@
 package roms;
 
 import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
 /**
@@ -17,8 +20,36 @@ public class MoneyTest {
         Money zero = new Money();
         assertEquals("0.00", zero.toString());
     }
+     
+    @Test
+    public void testStr() {
+        Money m = new Money("30");
+        assertEquals(m.toString(), "30.00");
+    }
     
-    /*
+    @Test
+    public void testAdd() {
+        Money d = new Money("30");
+        Money f = new Money("40");
+        assertEquals("70.00", d.add(f).toString());
+    }
+    
+        @Test
+    public void testMult() {
+        Money d = new Money("3");
+        assertEquals("21.00", d.multiply(7).toString());
+    }
+        
+         @Test
+    public void testPerc() {
+        Money d = new Money("2");
+        assertEquals("7.00", d.addPercent(250).toString());
+    }
+        
+  
+      
+      
+      /*
      ***********************************************************************
      * BEGIN MODIFICATION AREA
      ***********************************************************************
